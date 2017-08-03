301a51ad070108deeb5b013f5933203b109f0ef3
paj <pa.j@hotmail.fr>
2017-08-03 10:54:11 +0000

    Page modif profil

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :path=>'',
-                    :path_names=>{:sign_in=>'Login', :sign_out=>'Logout', :edit=>'Profile'}
+                    :path_names=>{:sign_in=>'Login', :sign_out=>'Logout', :edit=>'Profile'},
+                    :controllers => {:registrations => 'registrations' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
